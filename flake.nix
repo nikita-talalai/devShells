dcef30673e2d62496f13562225f6dc88cde1f7ae
Nikita Talalai <nikita.talalai.2002@gmail.com>
2024-07-10 02:24:58 +0300

    feat: add shell for c

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { pkgs, ... }: {
+
+        devShells.c = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            gdb
+          ];
+        };
+      };
+    };
+}
